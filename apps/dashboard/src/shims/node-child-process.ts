1c0d871992f2967b10bdedc41ea34b1c93169ba2
KONFeature <quentin@nivelais.com>
2026-04-09 13:07:39 +0200

    fix shim for new opencode version

--- apps/dashboard/src/shims/node-child-process.ts
+++ apps/dashboard/src/shims/node-child-process.ts
@@ -1,3 +1,6 @@
 export const spawn = () => {
   throw new Error("spawn is not available in browser");
 };
+export const spawnSync = () => {
+  throw new Error("spawnSync is not available in browser");
+};
